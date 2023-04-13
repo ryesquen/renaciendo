4e8803c9079086dcffe111c6dbbe5f5c8128e58e
Renzo Yesquén <ryesquen@gmail.com>
2023-04-13 18:09:50 -0500

    Validaciones

--- RenaciendoWebAPI/Controllers/RenacerController.cs
+++ RenaciendoWebAPI/Controllers/RenacerController.cs
@@ -1,6 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using RenaciendoWebAPI.Models;
+﻿using Microsoft.AspNetCore.Mvc;
+using RenaciendoWebAPI.Datos;
 using RenaciendoWebAPI.Models.Dtos;
 
 namespace RenaciendoWebAPI.Controllers
@@ -10,13 +9,41 @@ namespace RenaciendoWebAPI.Controllers
     public class RenacerController : ControllerBase
     {
         [HttpGet]
-        public IEnumerable<RenacerDTO> GetRenacer()
+        public ActionResult<IEnumerable<RenacerDTO>> GetRenacer()
         {
-            return new List<RenacerDTO>() {
-                new RenacerDTO { Nombre = "Nacimiento", RenacerId = 1},
-                new RenacerDTO { Nombre = "Desarrollo", RenacerId = 2},
-                new RenacerDTO { Nombre = "Más Crecimiento", RenacerId = 3}
-            };
+            return Ok(RenacerStore.renacerList);
+        }
+
+        [HttpGet("RenacerId:int", Name = "GetRenacerById")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(404)]
+        public ActionResult<RenacerDTO> GetRenacerById(int RenacerId)
+        {
+            if (RenacerId == 0) { return BadRequest(); }
+            var renacer = RenacerStore.renacerList.FirstOrDefault(r => r.RenacerId == RenacerId);
+            if (renacer == null) { return NotFound(); }
+            return Ok(renacer);
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public ActionResult<RenacerDTO> CrearRenacer([FromBody] RenacerDTO renacerDTO)
+        {
+            if (!ModelState.IsValid) { return BadRequest(renacerDTO); }
+            if (renacerDTO is null)
+            {
+                return BadRequest(renacerDTO);
+            }
+            if (renacerDTO.RenacerId > 0)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+            renacerDTO.RenacerId = RenacerStore.renacerList.OrderByDescending(r => r.RenacerId).First().RenacerId + 1;
+            RenacerStore.renacerList.Add(renacerDTO);
+            return CreatedAtRoute("GetRenacerById", new { RenacerId = renacerDTO.RenacerId }, renacerDTO);
         }
     }
 }
--- RenaciendoWebAPI/Models/Dtos/RenacerDTO.cs
+++ RenaciendoWebAPI/Models/Dtos/RenacerDTO.cs
@@ -1,8 +1,12 @@
-﻿namespace RenaciendoWebAPI.Models.Dtos
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RenaciendoWebAPI.Models.Dtos
 {
     public class RenacerDTO
     {
         public int RenacerId { get; set; }
+        [Required]
+        [MaxLength(30)]
         public string? Nombre { get; set; }
     }
 }
