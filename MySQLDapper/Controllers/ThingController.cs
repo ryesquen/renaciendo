1d45d421d7b1a743cfc8dc776f507475b6cc87a2
Renzo Yesquén <ryesquen@gmail.com>
2023-04-17 14:54:47 -0500

    get post dapper join

--- MySQLDapper/Controllers/ThingController.cs
+++ MySQLDapper/Controllers/ThingController.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Dapper;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using MySql.Data.MySqlClient;
+using MySQLDapper.Models;
 
 namespace MySQLDapper.Controllers
 {
@@ -10,8 +13,35 @@ namespace MySQLDapper.Controllers
         [HttpGet]
         public IActionResult Get()
         {
-            var query = "Select id, name, description from thing";
-            return Ok("hola");
+            var query = "Select Id, Nombre , Email  From Personas p;";
+            var cs = "Server = localhost; Port = 3306; Database = Dapper; Uid = root; Pwd = _RE41Nz000";
+
+            using var db = new MySqlConnection(cs);
+            return Ok(db.Query<Persona>(query).ToList());
+
+
+        }
+
+        [HttpGet("personasdirecciones")]
+        public IActionResult GetPersonasDirecciones()
+        {
+            var query = "Select p.Nombre , p.Email , d.Calle  From Direcciones d\r\nright join Personas p \r\non p.Id = d.PersonaId \r\nORDER BY 1";
+            var cs = "Server = localhost; Port = 3306; Database = Dapper; Uid = root; Pwd = _RE41Nz000";
+
+            using var db = new MySqlConnection(cs);
+            return Ok(db.Query(query).ToList());
+
+
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] Persona persona)
+        {
+            var cs = "Server = localhost; Port = 3306; Database = Dapper; Uid = root; Pwd = _RE41Nz000";
+            var query = "Insert INTO Personas (Nombre,Email) values (@nombre,@email);";
+            using var db = new MySqlConnection(cs);
+            var response = db.Execute(query, new { nombre = persona.Nombre, email = persona.Email });
+            return Ok(response);
         }
     }
 }
---
+++ MySQLDapper/Models/Persona.cs
@@ -0,0 +1,9 @@
+﻿namespace MySQLDapper.Models
+{
+    public class Persona
+    {
+        public int Id { get; set; }
+        public string? Nombre { get; set; }
+        public string? Email { get; set; }
+    }
+}
