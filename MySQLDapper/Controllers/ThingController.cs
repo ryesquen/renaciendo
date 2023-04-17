4b18f6a1f40d2e850744ae35544156107ec8d98e
Renzo Yesquén <ryesquen@gmail.com>
2023-04-17 13:46:36 -0500

    Dapper

---
+++ MySQLDapper/Controllers/ThingController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MySQLDapper.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ThingController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var query = "Select id, name, description from thing";
+            return Ok("hola");
+        }
+    }
+}
---
+++ MySQLDapper/Models/Thing.cs
@@ -0,0 +1,9 @@
+﻿namespace MySQLDapper.Models
+{
+    public class Thing
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+    }
+}
