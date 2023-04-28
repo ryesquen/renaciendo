acae7354ef9ff89ea16ca43e3e94036f71bdc0ac
Renzo Yesquén <ryesquen@gmail.com>
2023-04-28 08:01:49 -0500

    Azure Function 1

---
+++ az-local-http-trigger/Function1.cs
@@ -0,0 +1,35 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace az_local_http_trigger
+{
+    public static class Function1
+    {
+        [FunctionName("FuncionTest")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            string name = req.Query["name"];
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            dynamic data = JsonConvert.DeserializeObject(requestBody);
+            name = name ?? data?.name;
+
+            string responseMessage = string.IsNullOrEmpty(name)
+                ? "falta ..."
+                : $"Qué hay, {name}.?";
+
+            return new OkObjectResult(responseMessage);
+        }
+    }
+}
